bc78c1f6b9972592fb3df76dd84717137fa2dbab
Luke Roche <lr98lr@aol.com>
2016-10-26 11:25:11 -0400

    auto sized

--- desktop/src/com/intro/main/desktop/DesktopLauncher.java
+++ desktop/src/com/intro/main/desktop/DesktopLauncher.java
@@ -9,8 +9,6 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL30 = true;
 		config.vSyncEnabled = true;
-		config.width = 640;
-		config.height = 480;
 		new LwjglApplication(new ColorCore(), config);
 	}
 }
